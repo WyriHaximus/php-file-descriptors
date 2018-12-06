7c0cbba3dcc22d515641474b152a1c59d441e717
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-12-06 22:13:34 +0100

    ScanDir class to handle scanning of directories like /dev/fd and /proc/self/fd

---
+++ src/ScanDir.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\FileDescriptors;
+
+/**
+ * @internal
+ */
+final class ScanDir
+{
+    public static function scan(string $path): iterable
+    {
+        $fds = @\scandir($path, \SCANDIR_SORT_NONE);
+
+        if ($fds === false) {
+            throw new \Exception('Enable to list file descriptors: ' . \print_r(\error_get_last(), true));
+        }
+
+        foreach ($fds as $id) {
+            if (!\in_array($id, ['.', '..'], true)) {
+                yield $id;
+            }
+        }
+    }
+}
---
+++ tests/ScanDirTest.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\Tests\FileDescriptors;
+
+use ApiClients\Tools\TestUtilities\TestCase;
+use WyriHaximus\FileDescriptors\ScanDir;
+use function WyriHaximus\iteratorOrArrayToArray;
+
+/**
+ * @internal
+ */
+final class ScanDirTest extends TestCase
+{
+    public function testScan(): void
+    {
+        $fds = iteratorOrArrayToArray(ScanDir::scan(__DIR__));
+        self::assertCount(2, $fds);
+    }
+}
