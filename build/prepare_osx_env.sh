d1c3dda76023acabf81ef45a880efbf1d9a5905b
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-12-07 21:56:07 +0100

    Removed taps: https://travis-ci.com/WyriHaximus/php-file-descriptors/jobs/163750901#L1912-L1914

--- build/prepare_osx_env.sh
+++ build/prepare_osx_env.sh
@@ -6,14 +6,3 @@ brew --version
 
 echo "Updating brew..."
 brew update
-
-if [[ "${_PHP}" == "hhvm" ]]; then
-    echo "Adding brew HHVM dependencies..."
-    brew tap hhvm/hhvm
-
-else
-    echo "Adding brew PHP dependencies..."
-    brew tap homebrew/dupes
-    brew tap homebrew/versions
-    brew tap homebrew/homebrew-php
-fi
